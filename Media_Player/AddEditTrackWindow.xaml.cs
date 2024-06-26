7bc5191f40664e91dcd34de4901e735f1e69cfa3
Patryk Gamrat <Patryk.gam@gmail.com>
2024-06-26 19:51:19 +0200

    Edit tracks

--- Media_Player/AddEditTrackWindow.xaml.cs
+++ Media_Player/AddEditTrackWindow.xaml.cs
@@ -19,9 +19,22 @@ namespace Media_Player
     /// </summary>
     public partial class AddTrackWindow : Window
     {
-        public AddTrackWindow()
+        public AddTrackWindow(string mode)
         {
             InitializeComponent();
+            switch(mode) 
+            {
+                case "dodaj":
+                    this.Window.Title = "Dodaj nowy utwór";
+                    this.WindowLabel.Content = "DODAJ NOWY UTWÓR";
+                    this.AddEditButton.Content = "Dodaj";
+                    break;
+                case "edytuj":
+                    this.Window.Title = "Edytuj utwór";
+                    this.WindowLabel.Content = "EDYTUJ UTWÓR";
+                    this.AddEditButton.Content = "Edytuj";
+                    break;
+            }
         }
     }
 }
